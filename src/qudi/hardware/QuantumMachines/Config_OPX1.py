00f71814d82353a3bce7e1915a93c7dbb0832618
Amit Finkler <amit.finkler@weizmann.ac.il>
2025-12-01 16:04:10 +0200

    introducing a new pulser called OPX (compatible with QM OPX V2.2)

---
+++ src/qudi/hardware/QuantumMachines/Config_OPX1.py
@@ -0,0 +1,441 @@
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm.qua import *
+import numpy as np
+# from qm import SimulationConfig, ConfigHelper
+from qm import LoopbackInterface
+import matplotlib.pyplot as plt
+import time
+from scipy import special
+x = np.linspace(-3, 3,200)
+Rise_up = special.erf(x)*0.24+0.24
+Rise_down = special.erf(x)*(-0.24)+0.24
+noise_init = np.zeros(200)
+
+NV_IF = 150e6
+NV_LO = 1.8e9-NV_IF
+
+e_IF = 200e6
+e_LO = 1.4e9-e_IF
+
+# IF_freq = 150e6
+# rad_freq = 200e6
+# LO_freq = 1.5e9-IF_freq
+
+config = {
+
+    'version': 1,
+
+    'controllers': {
+
+        'finkler1': {
+            'type': 'opx1',
+            'analog_outputs': {
+                1: {'offset': 0.01407},  # I
+                2: {'offset': 0.01286},  # Q
+                3: {'offset': 0.01407},  # I
+                4: {'offset': 0.01286},  # Q
+                5: {'offset': 0}, #N
+                6: {'offset': 0}, #Noise
+            },
+            'digital_outputs': {
+                1: {'offset': 0.0},
+                2: {'offset': 0.0},
+                7: {'offset': 0.0},
+                8: {'offset': 0.0},
+                9: {'offset': 0.0},
+            },  # laser
+            'analog_inputs': {
+                1: {'offset': -0.032},  # readout1
+                2: {'offset': -0.037},  # readout2
+
+            }
+        }
+    },
+
+    'elements': {
+
+        'qubit': {
+            'mixInputs': {
+                'I': ('finkler1', 1),
+                'Q': ('finkler1', 2),
+                'lo_frequency': NV_LO,
+                'mixer': 'mixer_NV'
+            },
+            'intermediate_frequency': NV_IF,
+            # 'digitalInputs': {
+            #     "ttl_in": {
+            #         "port": ("finkler1", 7),
+            #         "delay": 136,
+            #         "buffer": 15,
+            #     },
+            #     "ttl_check": {
+            #         "port": ("finkler1", 8),
+            #         "delay": 136,
+            #         "buffer": 15,
+            #     },
+            # },
+            'operations': {
+                'MW_pi': 'MW_pi_pulse',
+            }
+        },
+        'qubit2': {
+            'mixInputs': {
+                'I': ('finkler1', 3),
+                'Q': ('finkler1', 4),
+                'lo_frequency': e_LO,
+                'mixer': 'mixer_e'
+            },
+            'intermediate_frequency': e_IF,
+            'digitalInputs': {
+                "ttl_in": {
+                    "port": ("finkler1", 7),
+                    "delay": 136,
+                    "buffer": 15,
+                },
+                "ttl_check": {
+                    "port": ("finkler1", 8),
+                    "delay": 136,
+                    "buffer": 15,
+                },
+            },
+            'operations': {
+                'MW_e': 'MW_e_pulse',
+            }
+        },
+
+        'radical': {
+            'mixInputs': {
+                'I': ('finkler1', 3),
+                'Q': ('finkler1', 4),
+                'lo_frequency': e_LO,
+                'mixer': 'mixer_e'
+            },
+            'intermediate_frequency': e_IF,
+            'digitalInputs': {
+                "ttl_in": {
+                    "port": ("finkler1", 7),
+                    "delay": 136,
+                    "buffer": 15,
+                },
+                "ttl_check": {
+                    "port": ("finkler1", 8),
+                    "delay": 136,
+                    "buffer": 15,
+                },
+            },
+            'operations': {
+                'MW_e': 'MW_e_pulse',
+            }
+        },
+
+        'nuclear': {
+            'singleInput': {
+                'port': ('finkler1', 5),
+                },
+            'intermediate_frequency': 300e6,
+
+            'operations': {
+                'nuclear_pi': 'nuclear_pi_pulse',
+                'nuclear_rise': 'nuclear_rise_pulse',
+                'nuclear_down': 'nuclear_down_pulse'
+            },
+            },
+
+        'noise': {
+            'singleInput': {
+                'port': ('finkler1', 6),
+                },
+            'intermediate_frequency': 0,
+            'operations': {
+                'inject_noise1': 'noise_pulse1',
+                'inject_noise2': 'noise_pulse2',
+                'inject_noise3': 'noise_pulse3'
+            },
+            },
+
+        'radical_2': {
+            'mixInputs': {
+                'I': ('finkler1', 1),
+                'Q': ('finkler1', 2),
+                'lo_frequency': NV_LO,
+                'mixer': 'mixer_NV'
+            },
+            'intermediate_frequency': NV_IF,
+            # 'digitalInputs': {
+            #     "ttl_in": {
+            #         "port": ("finkler1", 7),
+            #         "delay": 136,
+            #         "buffer": 15,
+            #     },
+            #     "ttl_check": {
+            #         "port": ("finkler1", 8),
+            #         "delay": 136,
+            #         "buffer": 15,
+            #     },
+            # },
+            'operations': {
+                'MW_pi': 'MW_pi_pulse',
+            }
+        },
+
+
+
+
+        "laser": {
+            'digitalInputs': {
+                "laser_in": {
+                    "port": ("finkler1", 1),
+                    "delay": 0,
+                    "buffer": 0,
+                },
+            },
+            'operations': {
+                'laser_pulse': 'laser_trig_pulse',
+            }
+        },
+
+        'readout1': {
+
+            # open: fake
+            "singleInput": {
+                "port": ("finkler1", 1)
+            },
+            # close: fake
+
+            # 'digitalInputs': {
+            #     "tt_in": {
+            #         "port": ("finkler1", 4),
+            #         "delay": 0,
+            #         "buffer": 0,
+            #     },
+            # },
+
+            'operations': {
+                'readout': 'readout_pulse',
+            },
+            "outputs": {
+                'out1': ("finkler1", 1)
+            },
+            'time_of_flight': 28,
+            'smearing': 0,
+            # 'outputPulse': [int(-arg) for arg in apd_pulse]
+            'outputPulseParameters': {
+                'signalThreshold': -300,
+                'signalPolarity': 'Descending',
+                'derivativeThreshold': -50,
+                'derivativePolarity': 'Descending'
+            }
+        },
+
+         'readout2': {
+
+             # open: fake
+             "singleInput": {
+                 "port": ("finkler1", 1)
+             },
+             # close: fake
+
+             # 'digitalInputs': {
+             #     "tt_in": {
+             #         "port": ("finkler1", 3),
+             #         "delay": 0,
+             #         "buffer": 0,
+             #     },
+             # },
+
+             'operations': {
+                 'readout': 'readout_pulse',
+             },
+             "outputs": {
+                 'out1': ("finkler1", 2)
+             },
+             'time_of_flight': 28,
+             'smearing': 0,
+             # 'outputPulse': [int(-arg) for arg in apd_pulse2]
+             'outputPulseParameters': {
+                'signalThreshold': -300,
+                'signalPolarity': 'Descending',
+                'derivativeThreshold': -50,
+                'derivativePolarity': 'Descending'}
+         },
+
+        "NI": {
+            'digitalInputs': {
+                "NI_in": {
+                    "port": ("finkler1", 2),
+                    "delay": 0,
+                    "buffer": 0,
+                },
+            },
+            'operations': {
+                'follow': 'follow_pulse',
+            }
+        },
+    },
+
+    "pulses": {
+        'MW_pi_pulse': {
+            'operation': "control",
+            'length': 24*4,
+            'waveforms': {
+                "I": "const_wf",
+                "Q": "zero_wf",
+            },
+            'digital_marker': 'ON'
+        },
+
+        'noise_pulse1': {
+            'operation': "control",
+            'length': 200,
+            'waveforms': {
+                'single': 'noise_wf1'
+            },
+        },
+
+        'noise_pulse2': {
+            'operation': "control",
+            'length': 200,
+            'waveforms': {
+                'single': 'noise_wf2'
+            },
+        },
+
+        'noise_pulse3': {
+            'operation': "control",
+            'length': 200,
+            'waveforms': {
+                'single': 'noise_wf3'
+            },
+        },
+
+        'MW_e_pulse': {
+            'operation': "control",
+            'length': 24*4,
+            'waveforms': {
+                "I": "const_wf",
+                "Q": "zero_wf"
+            },
+            'digital_marker': 'ON'
+        },
+
+        'MW_pi_pulse2': {
+            'operation': "control",
+            'length': 24*4,
+            'waveforms': {
+                "I": "const_wf",
+                "Q": "zero_wf"
+            },
+        },
+
+        'nuclear_pi_pulse': {
+            'operation': 'control',
+            'length': 24*4,
+            'waveforms': {
+                'single': 'const_nuclear_wf',
+            },
+            # 'digital_marker': 'ON'
+        },
+
+        'nuclear_rise_pulse': {
+            'operation': 'control',
+            'length': 200,
+            'waveforms': {
+                'single': 'rise_nuclear_wf',
+            },
+            # 'digital_marker': 'ON'
+        },
+
+        'nuclear_down_pulse': {
+            'operation': 'control',
+            'length': 200,
+            'waveforms': {
+                'single': 'down_nuclear_wf',
+            },
+            # 'digital_marker': 'ON'
+        },
+
+
+        'laser_trig_pulse': {
+            'operation': "control",
+            'length': 100,
+            'digital_marker': 'ON'
+        },
+
+        'follow_pulse': {
+            'operation': "control",
+            'length': 80,
+            'digital_marker': 'ON'
+        },
+
+        'readout_pulse': {
+            'operation': "measurement",
+            "length": 2000,
+            "waveforms": {
+                "single": "zero_wf"  # fake!
+            },
+            'digital_marker': 'ON',
+        },
+    },
+
+    'waveforms': {
+        "const_wf": {
+            "type": "constant",  # arbitrary give list og amps
+            "sample": 0.4  # need to give samples
+        },
+
+        "zero_wf": {
+            "type": "constant",
+            "sample": 0.0
+        },
+
+        'rise_nuclear_wf': {
+            "type": "arbitrary",  # arbitrary give list og amps
+            "samples": Rise_up
+        },
+
+        'down_nuclear_wf': {
+            "type": "arbitrary",  # arbitrary give list og amps
+            "samples": Rise_down
+        },
+
+        'const_nuclear_wf': {
+            "type": "constant",  # arbitrary give list og amps
+            "sample": 0.48 # need to give samples
+        },
+        'noise_wf1': {
+            "type": "arbitrary",  # arbitrary give list og amps
+            'is_overridable': True,
+            "samples": noise_init
+        },
+        'noise_wf2': {
+            "type": "arbitrary",  # arbitrary give list og amps
+            "samples": noise_init
+        },
+        'noise_wf3': {
+            "type": "arbitrary",  # arbitrary give list og amps
+            "samples": noise_init
+        },
+    },
+
+    "digital_waveforms": {
+        "ON": {
+            "samples": [(1, 0)]  # (on/off, ns)
+        }
+    },
+
+    "mixers": {
+        'mixer_NV': [
+            {'intermediate_frequency': NV_IF, 'lo_frequency': NV_LO, 'correction': [1.18,  0.19,  0.24,  0.96]}  # SSB (Both (-) left (+) right)
+            # {'intermediate_frequency': NV_IF, 'lo_frequency': NV_LO, 'correction': [1.0,  0.0,  0.0,  1.0]}  # SSB (Both (-) left (+) right)
+
+        ],
+        'mixer_e': [
+            {'intermediate_frequency': e_IF, 'lo_frequency': e_LO, 'correction': [1.18,  0.19,  0.24,  0.96]},  # SSB (Both (-) left (+) right)
+            # {'intermediate_frequency': e_IF, 'lo_frequency': e_LO, 'correction': [1.0,  0.0,  0.0,  1.0]}  # SSB (Both (-) left (+) right)
+
+        ]
+    },
+}
+
+# qmm = QuantumMachinesManager()
+# qm = qmm.open_qm(config)
---
+++ src/qudi/hardware/QuantumMachines/opx.py
@@ -0,0 +1,371 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file contains the Qudi hardware interface for pulsing devices.
+
+Copyright (c) 2021, the qudi developers. See the AUTHORS.md file at the top-level directory of this
+distribution and on <https://github.com/Ulm-IQO/qudi-iqo-modules/>
+
+This file is part of qudi.
+
+Qudi is free software: you can redistribute it and/or modify it under the terms of
+the GNU Lesser General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with qudi.
+If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import numpy as np
+from qm import QuantumMachinesManager
+from qudi.hardware.QuantumMachines.Config_OPX2 import config
+
+from qudi.core.configoption import ConfigOption
+from qudi.core.statusvariable import StatusVar
+from qudi.interface.pulser_interface import PulserInterface, PulserConstraints
+
+
+class OPX(PulserInterface):
+    """ Methods to control the QM OPX+ as a pulser
+
+    Example config for copy-paste:
+
+    opx:
+        module.Class: 'QuantumMachines.opx.OPX'
+        options:
+            opx_ip: '132.77.54.180'
+            cluster_name: 'Cluster_1'
+            config: 'QuantumMachines.Config.config'
+    """
+
+    _opx_ip = ConfigOption('opx_ip', '132.77.54.180', missing='warn')
+    _cluster_name = ConfigOption('cluster_name', 'Cluster_1', missing='warn')
+    _qmm = ''
+    _job = ''
+    __current_status = 0
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+    def on_activate(self):
+        """ Establish connection to pulse streamer and tell it to cancel all operations """
+        self._qmm = QuantumMachinesManager(host=self._opx_ip, cluster_name=self._cluster_name)  # should add the config file, too.
+        # self._qm = self._qmm.open_qm(config)
+
+    def on_deactivate(self):
+        self._qmm.close()
+        del self._qmm
+
+    def get_constraints(self):
+        """
+        Retrieve the hardware constrains from the Pulsing device.
+
+        @return constraints object: object with pulser constraints as attributes.
+
+        Provides all the constraints (e.g. sample_rate, amplitude, total_length_bins,
+        channel_config, ...) related to the pulse generator hardware to the caller.
+
+            SEE PulserConstraints CLASS IN pulser_interface.py FOR AVAILABLE CONSTRAINTS!!!
+
+        If you are not sure about the meaning, look in other hardware files to get an impression.
+        If still additional constraints are needed, then they have to be added to the
+        PulserConstraints class.
+
+        Each scalar parameter is an ScalarConstraints object defined in core.util.interfaces.
+        Essentially it contains min/max values as well as min step size, default value and unit of
+        the parameter.
+
+        PulserConstraints.activation_config differs, since it contain the channel
+        configuration/activation information of the form:
+            {<descriptor_str>: <channel_set>,
+             <descriptor_str>: <channel_set>,
+             ...}
+
+        If the constraints cannot be set in the pulsing hardware (e.g. because it might have no
+        sequence mode) just leave it out so that the default is used (only zeros).
+
+        # Example for configuration with default values:
+        constraints = PulserConstraints()
+
+        constraints.sample_rate.min = 10.0e6
+        constraints.sample_rate.max = 12.0e9
+        constraints.sample_rate.step = 10.0e6
+        constraints.sample_rate.default = 12.0e9
+
+        constraints.a_ch_amplitude.min = 0.02
+        constraints.a_ch_amplitude.max = 2.0
+        constraints.a_ch_amplitude.step = 0.001
+        constraints.a_ch_amplitude.default = 2.0
+
+        constraints.a_ch_offset.min = -1.0
+        constraints.a_ch_offset.max = 1.0
+        constraints.a_ch_offset.step = 0.001
+        constraints.a_ch_offset.default = 0.0
+
+        constraints.d_ch_low.min = -1.0
+        constraints.d_ch_low.max = 4.0
+        constraints.d_ch_low.step = 0.01
+        constraints.d_ch_low.default = 0.0
+
+        constraints.d_ch_high.min = 0.0
+        constraints.d_ch_high.max = 5.0
+        constraints.d_ch_high.step = 0.01
+        constraints.d_ch_high.default = 5.0
+
+        constraints.waveform_length.min = 80
+        constraints.waveform_length.max = 64800000
+        constraints.waveform_length.step = 1
+        constraints.waveform_length.default = 80
+
+        constraints.waveform_num.min = 1
+        constraints.waveform_num.max = 32000
+        constraints.waveform_num.step = 1
+        constraints.waveform_num.default = 1
+
+        constraints.sequence_num.min = 1
+        constraints.sequence_num.max = 8000
+        constraints.sequence_num.step = 1
+        constraints.sequence_num.default = 1
+
+        constraints.subsequence_num.min = 1
+        constraints.subsequence_num.max = 4000
+        constraints.subsequence_num.step = 1
+        constraints.subsequence_num.default = 1
+
+        # If sequencer mode is available then these should be specified
+        constraints.repetitions.min = 0
+        constraints.repetitions.max = 65539
+        constraints.repetitions.step = 1
+        constraints.repetitions.default = 0
+
+        constraints.event_triggers = ['A', 'B']
+        constraints.flags = ['A', 'B', 'C', 'D']
+
+        constraints.sequence_steps.min = 0
+        constraints.sequence_steps.max = 8000
+        constraints.sequence_steps.step = 1
+        constraints.sequence_steps.default = 0
+
+        # the name a_ch<num> and d_ch<num> are generic names, which describe UNAMBIGUOUSLY the
+        # channels. Here all possible channel configurations are stated, where only the generic
+        # names should be used. The names for the different configurations can be customary chosen.
+        activation_conf = OrderedDict()
+        activation_conf['yourconf'] = {'a_ch1', 'd_ch1', 'd_ch2', 'a_ch2', 'd_ch3', 'd_ch4'}
+        activation_conf['different_conf'] = {'a_ch1', 'd_ch1', 'd_ch2'}
+        activation_conf['something_else'] = {'a_ch2', 'd_ch3', 'd_ch4'}
+        constraints.activation_config = activation_conf
+        """
+        constraints = PulserConstraints()
+
+        # The file formats are hardware specific.
+
+        constraints.sample_rate.min = 1e9
+        constraints.sample_rate.max = 1e9
+        constraints.sample_rate.step = 0
+        constraints.sample_rate.default = 1e9
+
+        constraints.d_ch_low.min = 0.0
+        constraints.d_ch_low.max = 0.0
+        constraints.d_ch_low.step = 0.0
+        constraints.d_ch_low.default = 0.0
+
+        constraints.d_ch_high.min = 3.3
+        constraints.d_ch_high.max = 3.3
+        constraints.d_ch_high.step = 0.0
+        constraints.d_ch_high.default = 3.3
+
+        # sample file length max is not well-defined for PulseStreamer, which collates sequential
+        # identical pulses into one.
+        # Total number of not-sequentially-identical pulses which can be stored: 1 M.
+        constraints.waveform_length.min = 1
+        constraints.waveform_length.max = 134217728
+        constraints.waveform_length.step = 1
+        constraints.waveform_length.default = 1
+
+        # the name a_ch<num> and d_ch<num> are generic names, which describe UNAMBIGUOUSLY the
+        # channels. Here all possible channel configurations are stated, where only the generic
+        # names should be used. The names for the different configurations can be customary chosen.
+        activation_config = dict()
+        activation_config['all'] = frozenset(
+            {'d_ch1', 'd_ch2', 'd_ch3', 'd_ch4', 'd_ch5', 'd_ch6', 'd_ch7', 'd_ch8', 'd_ch9', 'd_ch10',
+             'a_ch1', 'a_ch2', 'a_ch3', 'a_ch4', 'a_ch5', 'a_ch6', 'a_ch7', 'a_ch8', 'a_ch9', 'a_ch10'}
+        )
+        constraints.activation_config = activation_config
+
+        return constraints
+
+    def pulser_on(self, prog):
+        """ Switches the pulsing device on.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        if prog:
+            self._job = self._qm.execute(prog, duration_limit=0, data_limit=0, flags=["skip-loop-unrolling"])
+            self.__current_status = 1
+            return 0
+        else:
+            self.log.error('no program was defined for the OPX')
+            self.pulser_off()
+            self.__current_status = -1
+            return -1
+
+    def pulser_off(self):
+        """ Switches the pulsing device off.
+
+        @return int: error code (0:OK, -1:error)
+        """
+        self.__current_status = 0
+        self._job.halt()
+        # self._qm.set_io1_value(1)  # this is what Inbar was using, but job.halt should be more appropriate
+        return
+
+    def load_waveform(self, load_dict):
+        """
+        In the OPX this will actually not do anything, as the "waveform" will be defined in the "prog" variable
+        defined in the predefined_methods
+        """
+        return
+
+    def get_loaded_assets(self):
+        """
+        Also here the OPX will show nothing, unless we write something.
+        """
+        return
+
+    def load_sequence(self, sequence_name):
+        """
+        Sequencing is not relevant for the OPX
+        """
+        self.log.debug('sequencing not relevant for the OPX')
+        return
+
+    def clear_all(self):
+        """
+        There is probably nothing stored in the OPX memory, so nothing to clear.
+        """
+        return
+
+    def get_status(self):
+        """
+        Retrieves the status of the OPX
+        """
+        return
+
+    def get_sample_rate(self):
+        """ Get the sample rate of the pulse generator hardware
+
+        @return float: The current sample rate of the device (in Hz)
+
+        Do not return a saved sample rate in a class variable, but instead
+        retrieve the current sample rate directly from the device.
+        """
+        return
+
+    def set_sample_rate(self, sample_rate):
+        """
+        not relevant for OPX
+        """
+        self.log.debug('OPX sample rate cannot be configured')
+        return
+
+    def get_analog_level(self, amplitude=None, offset=None):
+        """
+        eventually write something here
+        """
+        return
+
+    def set_analog_level(self, amplitude=None, offset=None):
+        """
+        eventually write something here
+        """
+        return
+
+    def get_digital_level(self, low=None, high=None):
+        """
+        eventually write something here
+        """
+        return
+
+    def set_digital_level(self, low=None, high=None):
+        """"
+        eventually write something here
+        """
+        return
+
+    def get_active_channels(self, ch=None):
+        """
+        eventually write something here
+        """
+        return
+    
+    def set_active_channels(self, ch=None):
+        """
+        eventually write something here
+        """
+        return
+
+    def write_waveform(self, name, analog_samples, digital_samples, is_first_chunk, is_last_chunk,
+                       total_number_of_samples):
+        """
+        eventually write something here
+        """
+        return
+
+    def write_sequence(self, name, sequence_parameters):
+        """
+        eventually write something here
+        """
+        return
+
+    def get_waveform_names(self):
+        """
+        eventually write something here
+        """
+        return
+
+    def get_sequence_names(self):
+        """
+        eventually write something here
+        """
+        return
+
+    def delete_waveform(self, waveform_name):
+        """
+        eventually write something here
+        """
+        return
+
+    def delete_sequence(self, sequence_name):
+        """
+        eventually write something here
+        """
+        return
+
+    def get_interleave(self):
+        """
+        eventually write something here
+        """
+        return False
+
+    def set_interleave(self, state=False):
+        """
+        eventually write something here
+        """
+        return False
+
+    def reset(self):
+        """
+        no such command for the OPX
+        """
+        return
+
+    def has_sequence_mode(self):
+        """
+        eventually write something here
+        """
+        return False
